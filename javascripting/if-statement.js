6b6c98892a84f6d6cce0efee848153d400d79e53
Ashutosh Dubey <dubeyashutosh306@gmail.com>
2021-09-27 16:54:37 +0530

    javascript demo

---
+++ javascripting/if-statement.js
@@ -0,0 +1,8 @@
+let fruit="orange";
+var length=fruit.length;
+if(length>5){
+	console.log("The fruit name has more than five characters.");
+}
+else{
+	console.log("The fruit name five characters or less.");
+}
